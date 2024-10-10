d17d03f39dc599077b6123a9c2af5be6b279a9d8
Nguyễn Minh Châu <chaunm27@fpt.com>
2024-10-10 17:39:11 +0700

    push code

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import React, { useState, useMemo } from 'react';
 
 export const Table = ({
-	limitPage = 5,
+	limitPage = 99999999,
 	columns = [],
 	data = [],
 	title = '',
