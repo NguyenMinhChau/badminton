b8005bbce9089004e4c174d68172c7cd6afdc4a6
Nguyen Minh Chau <chaunm27@fpt.com>
2024-10-11 10:28:28 +0700

    push code

--- src/components/TournamentBrackets.js
+++ src/components/TournamentBrackets.js
@@ -12,22 +12,6 @@ export const TournamentBrackets = ({
 	const CustomSeed = ({ seed, breakpoint, roundIndex, seedIndex }) => {
 		const score0 = Number(seed?.teams[0]?.score);
 		const score1 = Number(seed?.teams[1]?.score);
-		const isScore0HigherOrEqual = score0 >= score1;
-		const isScore1HigherOrEqual = score1 >= score0;
-
-		const backgroundPosition0 = isScore0HigherOrEqual
-			? 'bg-green-500'
-			: 'bg-gray-500';
-		const colorPosition0 = isScore0HigherOrEqual
-			? 'text-green-500'
-			: 'text-gray-500';
-
-		const backgroundPosition1 = isScore1HigherOrEqual
-			? 'bg-green-500'
-			: 'bg-gray-500';
-		const colorPosition1 = isScore1HigherOrEqual
-			? 'text-green-500'
-			: 'text-gray-500';
 		return (
 			<Seed mobileBreakpoint={breakpoint} style={{ fontSize: 12 }}>
 				<SeedItem>
@@ -35,34 +19,44 @@ export const TournamentBrackets = ({
 						<div className="text-center text-orange-500 mt-1 capitalize">
 							{seed?.date}
 						</div>
-						<div className="flex flex-col gap-3 my-2">
-							<div className="flex flex-row gap-1 items-start">
-								<div className="flex-1 flex flex-col items-start">
-									<input
-										type="text"
-										defaultValue={
-											seed.teams[0]?.name || 'NO TEAM '
-										}
-										onBlur={(e) => {
-											handleChangeSeedName(
-												roundIndex,
-												seedIndex,
-												0,
-												e.target.value,
-												paramsHandleChangeSeedName,
-											);
-										}}
-										style={{
-											width: '-webkit-fill-available',
-										}}
-										className={`bg-opacity-30 outline-none border-none py-2 text-[14px] mx-1 ${colorPosition0} ${backgroundPosition0} font-bold p-[2px] rounded-tl-md rounded-br-md text-center`}
-									/>
-									<span className="font-bold mx-2 text-[8px] italic">
-										Phòng Ban:{' '}
-										{seed?.teams[0]?.department || '---'}
-									</span>
-								</div>
-								<input
+						<div
+							className="flex flex-row gap-1 my-2"
+							style={{
+								alignItems: seed.teams[1]?.name
+									? 'center'
+									: 'start',
+							}}
+						>
+							<div className="flex flex-col gap-3">
+								<div className="flex flex-row gap-1 items-start">
+									<div className="flex-1 flex flex-col items-start">
+										<input
+											type="text"
+											defaultValue={
+												seed.teams[0]?.name ||
+												'NO TEAM '
+											}
+											onBlur={(e) => {
+												handleChangeSeedName(
+													roundIndex,
+													seedIndex,
+													0,
+													e.target.value,
+													paramsHandleChangeSeedName,
+												);
+											}}
+											style={{
+												width: '-webkit-fill-available',
+											}}
+											className={`bg-opacity-30 outline-none border-none py-2 text-[14px] mx-1 text-green-500 bg-green-500 font-bold p-[2px] rounded-tl-md rounded-br-md text-center`}
+										/>
+										<span className="font-bold mx-2 text-[8px] italic">
+											Phòng Ban:{' '}
+											{seed?.teams[0]?.department ||
+												'---'}
+										</span>
+									</div>
+									{/* <input
 									type="text"
 									defaultValue={score0?.toString()}
 									onBlur={(e) => {
@@ -74,51 +68,66 @@ export const TournamentBrackets = ({
 											paramsHandleChangeSeedScore,
 										);
 									}}
-									className={`w-[50px] bg-opacity-30 py-2 outline-none border-none text-[14px] mr-1 ${colorPosition0} ${backgroundPosition0} font-bold p-[2px] rounded-tr-md rounded-bl-md text-center`}
-								/>
-							</div>
-							{seed.teams[1]?.name && (
-								<div className="flex flex-row gap-1 items-start">
-									<div className="flex flex-col items-start">
-										<input
+									className={`w-[50px] bg-opacity-30 py-2 outline-none border-none text-[14px] mr-1 text-green-500 bg-green-500 font-bold p-[2px] rounded-tr-md rounded-bl-md text-center`}
+								/> */}
+								</div>
+								{seed.teams[1]?.name && (
+									<div className="flex flex-row gap-1 items-start">
+										<div className="flex flex-col items-start">
+											<input
+												type="text"
+												defaultValue={
+													seed.teams[1]?.name ||
+													'NO TEAM '
+												}
+												onBlur={(e) => {
+													handleChangeSeedName(
+														roundIndex,
+														seedIndex,
+														1,
+														e.target.value,
+														paramsHandleChangeSeedName,
+													);
+												}}
+												className={`bg-opacity-30 py-2 outline-none border-none text-[14px] mx-1 text-green-500 bg-green-500 font-bold p-[2px] rounded-tl-md rounded-br-md text-center`}
+											/>
+											<span className="font-bold mx-2 text-[8px] italic">
+												Phòng Ban:{' '}
+												{seed?.teams[1]?.department ||
+													'---'}
+											</span>
+										</div>
+										{/* <input
 											type="text"
-											defaultValue={
-												seed.teams[1]?.name ||
-												'NO TEAM '
-											}
+											defaultValue={score1?.toString()}
 											onBlur={(e) => {
-												handleChangeSeedName(
+												handleChangeSeedScore(
 													roundIndex,
 													seedIndex,
 													1,
 													e.target.value,
-													paramsHandleChangeSeedName,
+													paramsHandleChangeSeedScore,
 												);
 											}}
-											className={`bg-opacity-30 py-2 outline-none border-none text-[14px] mx-1 ${colorPosition1} ${backgroundPosition1} font-bold p-[2px] rounded-tl-md rounded-br-md text-center`}
-										/>
-										<span className="font-bold mx-2 text-[8px] italic">
-											Phòng Ban:{' '}
-											{seed?.teams[1]?.department ||
-												'---'}
-										</span>
+											className={`w-[50px] py-2 bg-opacity-30 outline-none border-none text-[14px] mr-1 text-green-500 bg-green-500 font-bold p-[2px] rounded-tr-md rounded-bl-md text-center`}
+										/> */}
 									</div>
-									<input
-										type="text"
-										defaultValue={score1?.toString()}
-										onBlur={(e) => {
-											handleChangeSeedScore(
-												roundIndex,
-												seedIndex,
-												1,
-												e.target.value,
-												paramsHandleChangeSeedScore,
-											);
-										}}
-										className={`w-[50px] py-2 bg-opacity-30 outline-none border-none text-[14px] mr-1 ${colorPosition1} ${backgroundPosition1} font-bold p-[2px] rounded-tr-md rounded-bl-md text-center`}
-									/>
-								</div>
-							)}
+								)}
+							</div>
+							<input
+								type="text"
+								defaultValue={score0?.toString()}
+								onBlur={(e) => {
+									handleChangeSeedScore(
+										roundIndex,
+										seedIndex,
+										0,
+										e.target.value,
+										paramsHandleChangeSeedScore,
+									);
+								}}
+								className={`w-[50px] bg-opacity-30 py-2 outline-none border-none text-[14px] mr-1 text-green-500 bg-green-500 font-bold p-[2px] rounded-tr-md rounded-bl-md text-center`}
+							/>
 						</div>
 					</div>
 				</SeedItem>
