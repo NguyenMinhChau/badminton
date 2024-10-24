5bc8644bcde12e0864de0d8f1cc56333895025c8
Nguyen Minh Chau <chaunm27@fpt.com>
2024-10-24 12:49:23 +0700

    push code

--- src/components/TournamentBrackets.js
+++ src/components/TournamentBrackets.js
@@ -95,13 +95,13 @@ export const TournamentBrackets = ({
 											paramsFunc,
 										);
 									}}
-									className={`w-full px-2 pt-1 bg-white text-orange-700 uppercase outline-none font-bold border-none text-[13px] text-center`}
+									className={`w-full px-2 py-1 bg-[#ea580c] text-white uppercase outline-none font-bold border-none text-[13px] text-center`}
 									disabled={_checkDisabled || disabled}
 									readOnly={_checkDisabled || disabled}
 								/>
 							)}
 							<div
-								className="flex flex-row gap-1 m-2"
+								className="flex flex-row gap-1 m-2 mt-[2px]"
 								style={{
 									alignItems: seed.teams?.[1]?.name ? 'center' : 'start',
 								}}
